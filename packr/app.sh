9ae07600eea0167c9659c3ac6938347642620d20
Rowell Belen <rbelen@pentaho.com>
2014-07-24 10:51:27 -0400

    instructions to use embedded JRE

--- packr/app.sh
+++ packr/app.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 BASEDIR=`dirname $0`
-exec java \
+exec chmod +x $BASEDIR/jre/bin/*
+
+exec $BASEDIR/jre/bin/java \
      -d64 \
      -XstartOnFirstThread \
      -jar $BASEDIR/uber-thin-web-desktop-1.0-SNAPSHOT.jar
